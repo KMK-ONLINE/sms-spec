a767098a192dfba1f55aeb77655494ac58e1bd4d
Sukoreno Mukti Widodo <sukorenomw@gmail.com>
2017-12-13 11:48:23 +0700

    Fix unload driver for twilio-ruby > 5.0

--- lib/sms_spec/drivers/unloads/twilio-ruby.rb
+++ lib/sms_spec/drivers/unloads/twilio-ruby.rb
@@ -1,14 +1,16 @@
 module Twilio
   module REST
-    class Client
-      alias_method :initialize_from_driver, :initialize
-      alias_method :initialize,             :initialize_original
-
+    class Api
       alias_method :account_from_driver, :account
       alias_method :account,             :account_original
 
       remove_const :Messages
       remove_const :Account
+    end
+
+    class Client
+      alias_method :initialize_from_driver, :initialize
+      alias_method :initialize,             :initialize_original
     end
   end
 end
